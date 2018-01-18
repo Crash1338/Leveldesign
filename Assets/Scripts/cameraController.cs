3380274bf3815ee6e8ddb70eb928ffaf2f908b4e
Marcel <marcelaporius@gmx.de>
2018-01-18 02:23:15 +0100

    start

---
+++ Assets/Scripts/cameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour {
+
+    public GameObject player;
+    public float turnspeed;
+    public Vector3 offset;
+
+    void Start ()
+    {
+        
+    }
+
+    void LateUpdate () {
+
+        transform.position = player.transform.position + offset;
+        transform.LookAt(player.transform.position);
+
+    }
+}
---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+    public float ballSpeed;
+    private Rigidbody body;
+    
+
+   
+    // Use this for initialization
+    void Start () {
+        body = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float xSpeed = Input.GetAxis("Horizontal");
+        float ySpeed = Input.GetAxis("Vertical");
+             
+        
+        body.AddForce (new Vector3(xSpeed, 0, ySpeed) * ballSpeed * Time.deltaTime);
+    } 
+	
+}
